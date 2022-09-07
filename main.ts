969e7f8f6fbdf826a12c9ad85a0e15bece5573d5
ghizboi <113055346+ghizboi@users.noreply.github.com>
2022-09-07 14:11:20 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(1)
+    basic.showNumber(2)
+    basic.showNumber(3)
+    basic.showString("BEGIN")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("GAME OVER")
+})
+basic.forever(function () {
+	
+})
